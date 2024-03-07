6359b815419846909f6924fa99d04e29ec4430cf
olvallej <olvallej@unirioja.es>
2024-03-08 00:13:31 +0100

    fuente de datos cargada y corregida

--- app/src/main/java/com/example/cuadricula/data/DataSource.kt
+++ app/src/main/java/com/example/cuadricula/data/DataSource.kt
@@ -1,27 +1,24 @@
 package com.example.cuadricula.data
 
-import android.adservices.topics.Topic
-import android.os.ext.SdkExtensions
-import androidx.annotation.RequiresExtension
 import com.example.cuadricula.R
+import com.example.cuadricula.model.Topic
 
 
 object DataSource {
-    @RequiresExtension(extension = SdkExtensions.AD_SERVICES, version = 4)
     val topics = listOf(
-        Topic(R.string.architecture.toLong(), 58, R.drawable.architecture),
-        Topic(R.string.crafts.toLong(), 121, R.drawable.crafts),
-        Topic(R.string.business.toLong(), 78, R.drawable.business),
-        Topic(R.string.culinary.toLong(), 118, R.drawable.culinary),
-        Topic(R.string.design.toLong(), 423, R.drawable.design),
-        Topic(R.string.fashion.toLong(), 92, R.drawable.fashion),
-        Topic(R.string.film.toLong(), 165, R.drawable.film),
-        Topic(R.string.gaming.toLong(), 164, R.drawable.gaming),
-        Topic(R.string.drawing.toLong(), 326, R.drawable.drawing),
-        Topic(R.string.lifestyle.toLong(), 305, R.drawable.lifestyle),
-        Topic(R.string.music.toLong(), 212, R.drawable.music),
-        Topic(R.string.painting.toLong(), 172, R.drawable.painting),
-        Topic(R.string.photography.toLong(), 321, R.drawable.photography),
-        Topic(R.string.tech.toLong(), 118, R.drawable.tech)
+        Topic(R.string.architecture, 58, R.drawable.architecture),
+        Topic(R.string.crafts, 121, R.drawable.crafts),
+        Topic(R.string.business, 78, R.drawable.business),
+        Topic(R.string.culinary, 118, R.drawable.culinary),
+        Topic(R.string.design, 423, R.drawable.design),
+        Topic(R.string.fashion, 92, R.drawable.fashion),
+        Topic(R.string.film, 165, R.drawable.film),
+        Topic(R.string.gaming, 164, R.drawable.gaming),
+        Topic(R.string.drawing, 326, R.drawable.drawing),
+        Topic(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Topic(R.string.music, 212, R.drawable.music),
+        Topic(R.string.painting, 172, R.drawable.painting),
+        Topic(R.string.photography, 321, R.drawable.photography),
+        Topic(R.string.tech, 118, R.drawable.tech)
     )
 }
